bd9e4636266fd5d8a9d018bef4161c68ec54d6f7
Dhaval Vaghani <dhaval.vaghani@domesticandgeneral.com>
2021-09-28 09:57:09 +0100

    bug fix

--- libs/transactionsviewer/data-access-transactions/src/lib/transactions-api.service.ts
+++ libs/transactionsviewer/data-access-transactions/src/lib/transactions-api.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { TransactionsApiResponse } from '@transactionsviewer/util-models';
-import { catchError, delay } from 'rxjs/operators';
-import { of } from 'rxjs';
-import { MOCK_RESPONSE } from './mock-data';
 
 @Injectable()
 export class TransactionsApiService {
@@ -17,23 +14,18 @@ export class TransactionsApiService {
     page: number
   ) {
     let params = new HttpParams();
-    params.append('page', page);
+    params = params.append('page', page);
     params = dateFilter ? params.append('date', dateFilter) : params;
     params = statusFilter ? params.append('status', statusFilter) : params;
 
-    return this.httpClient
-      .get<TransactionsApiResponse>(`${this.api}/payments`, {
+    return this.httpClient.get<TransactionsApiResponse>(
+      `${this.api}/payments`,
+      {
+        params: params,
         headers: {
-          ...this.getHeaders(),
+          Authorization: 'Basic dXNlcjp1c2VyUGFzcw==',
         },
-        params,
-      })
-      .pipe(catchError(() => of(MOCK_RESPONSE).pipe(delay(500))));
-  }
-
-  private getHeaders() {
-    return {
-      Authorization: 'Basic dXNlcjp1c2VyUGFzcw==',
-    };
+      }
+    );
   }
 }
